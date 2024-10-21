3b126b58fa8736094d16cadc61810883df1d9113
Andy Jimenez <a17100657@gmail.com>
2024-10-20 19:46:22 -0600

    Corregir

--- app/src/main/java/com/example/routemap/MainActivity.kt
+++ app/src/main/java/com/example/routemap/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.routemap
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -11,7 +11,6 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
-import com.example.routemap.Utils.GPSController
 import com.example.routemap.Utils.Utils
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,6 +18,8 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
 import com.google.android.gms.maps.model.PolylineOptions
 import org.json.JSONArray
 import org.json.JSONObject
@@ -28,20 +29,34 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
 
     private lateinit var nMap: GoogleMap
     private lateinit var request: RequestQueue
-    private lateinit var gpsTracker: GPSController
+    private var polyline: Polyline? = null
+    private var isOriginSet = false
+    private var originLatLng: LatLng? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gpsTracker = GPSController(applicationContext)
-
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         request = Volley.newRequestQueue(applicationContext)
-    }
 
+        val btnNuevaRuta: Button = findViewById(R.id.btnNuevaRuta)
+        btnNuevaRuta.setOnClickListener {
+            nuevaRuta()
+        }
+    }
+    private fun nuevaRuta() {
+        nMap.clear()
+
+        Utils.markersDefault(nMap, applicationContext)
+
+        polyline = null
+        isOriginSet = false
+
+        Toast.makeText(this, "Seleccione una nueva ruta.", Toast.LENGTH_SHORT).show()
+    }
     override fun onMapReady(map: GoogleMap) {
         nMap = map
         if (ActivityCompat.checkSelfPermission(
@@ -64,35 +79,45 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
         nMap.setOnMarkerClickListener(this)
         nMap.setOnMapLongClickListener(this)
     }
-
     override fun onMapLongClick(latLng: LatLng) {
-        Utils.coordenadas.origenLat = latLng.latitude
-        Utils.coordenadas.origenLong = latLng.longitude
-        Toast.makeText(this, "Toque el icono para que selecciones", Toast.LENGTH_SHORT).show()
-
-        // Para obtener un destino de ejemplo
-        val destination = LatLng(20.6744, -103.3925) // Cambia esto al destino que desees
-        fetchDirections(Utils.coordenadas.origenLat, Utils.coordenadas.origenLong, destination)
+        if (!isOriginSet) {
+            originLatLng = latLng
+            Utils.coordenadas.origenLat = latLng.latitude
+            Utils.coordenadas.origenLong = latLng.longitude
+            Toast.makeText(this, "Origen establecido", Toast.LENGTH_SHORT).show()
+            isOriginSet = true
+        } else {
+            val destinationLatLng = latLng
+            Toast.makeText(this, "Destino establecido", Toast.LENGTH_SHORT).show()
+            if (originLatLng != null) {
+                fetchDirections(originLatLng!!.latitude, originLatLng!!.longitude, destinationLatLng)
+            }
+            isOriginSet = false
+        }
     }
-
     private fun fetchDirections(originLat: Double, originLng: Double, destination: LatLng) {
-        val url = "https://maps.googleapis.com/maps/api/directions/json?origin=$originLat,$originLng&destination=${destination.latitude},${destination.longitude}&key=AIzaSyC9RMr64zwG_deCUGkVGMx35gNwV3vn4Wk"
+        val url = "https://maps.googleapis.com/maps/api/directions/json?" +
+                "origin=$originLat,$originLng&destination=${destination.latitude},${destination.longitude}&key=AIzaSyC9RMr64zwG_deCUGkVGMx35gNwV3vn4Wk"
 
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { response ->
-                // Procesar la respuesta JSON aquí
                 val routePoints = parseRoute(response)
                 drawRoute(routePoints)
+                nMap.addMarker(
+                    MarkerOptions().position(LatLng(originLat, originLng)).title("Origen")
+                )
+                nMap.addMarker(MarkerOptions().position(destination).title("Destino"))
             },
             { error ->
-                // Manejo de errores
-                Toast.makeText(this, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Error al obtener la ruta: ${error.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         )
-
         request.add(jsonObjectRequest)
     }
-
     private fun parseRoute(response: JSONObject): List<LatLng> {
         val routePoints = mutableListOf<LatLng>()
         val routes: JSONArray = response.getJSONArray("routes")
@@ -109,7 +134,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
         }
         return routePoints
     }
-
     private fun decodePoly(encoded: String): List<LatLng> {
         val poly = mutableListOf<LatLng>()
         var index = 0
@@ -147,32 +171,35 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback,
         }
         return poly
     }
-
     override fun onMapClick(p0: LatLng) {
-        // Manejar clic en el mapa si es necesario
         Toast.makeText(this, "Clic en el mapa: ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
     }
-
     override fun onMarkerClick(marker: Marker): Boolean {
-        Toast.makeText(this, "Marcador: ${marker.title}", Toast.LENGTH_SHORT).show()
-        return false // Retornar false para permitir el comportamiento por defecto
+        if (!isOriginSet) {
+            Utils.coordenadas.origenLat = marker.position.latitude
+            Utils.coordenadas.origenLong = marker.position.longitude
+            originLatLng = marker.position
+            isOriginSet = true
+            Toast.makeText(this, "Origen establecido", Toast.LENGTH_SHORT).show()
+        } else {
+            val destinationLatLng = marker.position
+            Toast.makeText(this, "Destino establecido", Toast.LENGTH_SHORT).show()
+            fetchDirections(originLatLng!!.latitude, originLatLng!!.longitude, destinationLatLng)
+            isOriginSet = false
+        }
+
+        return true
     }
-
     private fun drawRoute(routePoints: List<LatLng>) {
-        // Elimina cualquier ruta existente antes de dibujar la nueva
-        nMap.clear()
+        polyline?.remove()
 
-        // Dibuja la nueva ruta en el mapa
         val polylineOptions = PolylineOptions()
-            .width(10f) // Ancho de la línea
+            .width(10f)
             .color(getColor(R.color.colorRoute))
 
-        // Añade los puntos de la ruta a la polyline
         for (point in routePoints) {
             polylineOptions.add(point)
         }
-
-        // Agrega la polyline al mapa
-        nMap.addPolyline(polylineOptions)
+        polyline = nMap.addPolyline(polylineOptions)
     }
 }
--- app/src/main/java/com/example/routemap/Utils/Utils.kt
+++ app/src/main/java/com/example/routemap/Utils/Utils.kt
@@ -6,8 +6,6 @@ import com.google.android.gms.maps.GoogleMap
 class Utils {
     companion object {
         var coordenadas = Coordenadas()
-        var routes = mutableListOf<List<HashMap<String, String>>>()
-
         fun markersDefault(nMap: GoogleMap, context: Context) {
             Marcadores(nMap, context).addMarkersDefault()
         }
